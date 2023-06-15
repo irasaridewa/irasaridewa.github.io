9c7724e94b0e156fc7f8f3ab37ac882ccf4c3878
Noor Maulida <noor.maulida@cognotiv.com>
2023-06-15 20:35:38 +0700

    Update portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -207,15 +207,6 @@
   });
 
   /**
-   * Initiate portfolio details lightbox 
-   */
-  const portfolioDetailsLightbox = GLightbox({
-    selector: '.portfolio-details-lightbox',
-    width: '90%',
-    height: '90vh'
-  });
-
-  /**
    * Portfolio details slider
    */
   new Swiper('.portfolio-details-slider', {
